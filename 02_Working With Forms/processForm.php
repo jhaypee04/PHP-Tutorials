430a9187e02cd1cbc98ff6bf64f6540ad9c28668
EDOZIE JOHNPAUL UZOMA <107867208+loctechng@users.noreply.github.com>
2022-08-09 16:55:18 +0100

    Array and working with forms

--- 02_Working With Forms/processForm.php
+++ 02_Working With Forms/processForm.php
@@ -5,5 +5,13 @@
     Welcome <?php echo $_POST["name"]; ?>   <br>
     Your email address is: <?php echo $_POST["email"]; ?>
     
+
+    <?php
+//    Redirecting the user
+    
+  header("Location: http://www.tutorialspoint.com");
+//   exit;
+?>
+
 </body>
 </html>
